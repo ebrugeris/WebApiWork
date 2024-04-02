df6e7c97c01eea5db7b6c9e8d2a50415778d9843
Ebru Ünlü <ebruunlu3@gmail.com>
2024-04-03 00:36:49 +0300

    initial commit

--- WebApiWork/Controllers/AdminUserController.cs
+++ WebApiWork/Controllers/AdminUserController.cs
@@ -11,22 +11,69 @@ namespace WebApiWork.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            AdminUserContext db= new AdminUserContext();
-            List<AdminUser> adminUsers= db.AdminUsers.ToList();
+            AdminUserContext db = new AdminUserContext();
+            List<AdminUser> adminUsers = db.AdminUsers.ToList();
             return Ok(adminUsers);
         }
 
         [HttpGet("{id}")]
         public IActionResult GetById(int id)
         {
-            AdminUserContext db= new AdminUserContext();
-            AdminUser adminUser= db.AdminUsers.FirstOrDefault(x=> x.Id == id );
+            AdminUserContext db = new AdminUserContext();
+            AdminUser adminUser = db.AdminUsers.FirstOrDefault(x => x.Id == id);
 
-            if (adminUser==null)
+            if (adminUser == null)
             {
-                return NotFound("Böyle bir kullanıcı mevcut değil.");
+                return NotFound("Böyle bir kullanıcı bulunamadı.");
             }
             return Ok(adminUser);
         }
+
+        [HttpPost]
+        public IActionResult Post(AdminUser adminUser)
+        {
+            AdminUserContext db = new AdminUserContext();
+            db.AdminUsers.Add(adminUser);
+            db.SaveChanges();
+
+            return Ok("Ekleme islemi basarili.");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            AdminUserContext db = new AdminUserContext();
+            AdminUser adminUser = db.AdminUsers.Find(id);
+
+            if (adminUser==null)
+            {
+                return NotFound("Böyle bir kullanıcı bulunamadı.");
+            }
+
+            db.AdminUsers.Remove(adminUser);
+            db.SaveChanges();
+            return Ok("İşlem başarılı");
+        }
+
+        [HttpPut]
+        public IActionResult Put(AdminUser adminUser)
+        {
+            AdminUserContext db = new AdminUserContext();
+            AdminUser entity = db.AdminUsers.FirstOrDefault(x=> x.Id== adminUser.Id);
+
+            entity.UserName=adminUser.UserName;
+            entity.Surname=adminUser.Surname;
+            entity.Email = adminUser.Email;
+            entity.Phone=adminUser.Phone;
+            entity.AddDate= adminUser.AddDate;
+            entity.IsDeleted=adminUser.IsDeleted;
+
+            db.SaveChanges();
+
+            return Ok("Guncelleme islemi basarili.");
+
+
+        }
+
     }
 }
